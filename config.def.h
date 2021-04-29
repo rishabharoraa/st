5a8efe75b20d3585b114b89acdae77969974f07a
RishabhAroraa <rishabharora.ns@gmail.com>
2021-04-29 23:50:21 +0530

    you tell me

--- config.def.h
+++ config.def.h
@@ -5,9 +5,9 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "FiraCode Nerd Font Mono:pixelsize=16:antialias=true:autohint=true";
+static char *font = "FiraCode Nerd Font Mono:pixelsize=18:antialias=true:autohint=true";
 static char *font2[] = {
-	"JoyPixels:pixelsize=16:antialias=true:autohint=true",
+	"JoyPixels:pixelsize=18:antialias=true:autohint=true",
 };
 static int borderpx = 2;
 
@@ -181,6 +181,8 @@ static uint forcemousemod = ShiftMask;
  */
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release */
+	{ XK_ANY_MOD,           Button4, kscrollup,      {.i = 1},      0, /* !alt */ -1 },
+	{ XK_ANY_MOD,           Button5, kscrolldown,    {.i = 1},      0, /* !alt */ -1 },
 	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
 	{ ShiftMask,            Button4, ttysend,        {.s = "\033[5;2~"} },
 	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
@@ -206,6 +208,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
+	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
 };
 
 /*
